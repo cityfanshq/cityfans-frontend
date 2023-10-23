0aafcedb4c7a3f0574c67d7aaa5062a6f7990921
Tassia Pellegrini <tassia@lokaal>
2023-10-23 09:51:43 +0200

    scroller and city cards done

--- html/src/assets/js/scripts.js
+++ html/src/assets/js/scripts.js
@@ -32,3 +32,25 @@ window.addEventListener("load", function() {
 
 // OR USE GLIDER
 //https://nickpiscitelli.github.io/Glider.js/
+
+
+//SCROLLER
+const scrollers = document.querySelectorAll(".scroller");
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
