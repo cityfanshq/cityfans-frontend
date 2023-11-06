520207d20f86a479109e6b66b7ac0df021a7d6ae
Tassia Pellegrini <tassia@lokaal>
2023-11-06 20:08:25 +0100

    added the FAQ page with an accordion

--- html/src/assets/js/scripts.js
+++ html/src/assets/js/scripts.js
@@ -1,3 +1,56 @@
+//ACCORDIONS
+class Accordion {
+  constructor(domNode) {
+    this.rootEl = domNode;
+    this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
+
+    const controlsId = this.buttonEl.getAttribute('aria-controls');
+    this.contentEl = document.getElementById(controlsId);
+
+    this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
+
+    // add event listeners
+    this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
+  }
+
+  onButtonClick() {
+    this.toggle(!this.open);
+  }
+
+  toggle(open) {
+    // don't do anything if the open state doesn't change
+    if (open === this.open) {
+      return;
+    }
+
+    // update the internal state
+    this.open = open;
+
+    // handle DOM updates
+    this.buttonEl.setAttribute('aria-expanded', `${open}`);
+    if (open) {
+      this.contentEl.removeAttribute('hidden');
+    } else {
+      this.contentEl.setAttribute('hidden', '');
+    }
+  }
+
+  // Add public open and close methods for convenience
+  open() {
+    this.toggle(true);
+  }
+
+  close() {
+    this.toggle(false);
+  }
+}
+
+// init accordions
+const accordions = document.querySelectorAll('.accordion h3');
+accordions.forEach((accordionEl) => {
+  new Accordion(accordionEl);
+});
+
 //TABS
 window.addEventListener("load", function() {
   // store tabs variable
@@ -107,6 +160,10 @@ function initProgressBar() {
 
 $(ui.play).addEventListener('click', togglePlay);
 $(ui.audio).addEventListener('timeupdate', initProgressBar);
+
+
+
+
 
 //PRODUCT PAGE CAROUSEL
 "use strict";
